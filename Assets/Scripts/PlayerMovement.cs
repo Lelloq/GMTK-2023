8c9c5c67aef724aa31d71f0cc1ad19312f175a59
Gary Tang <46045360+Lelloq@users.noreply.github.com>
2023-07-07 18:56:27 +0100

    fixed stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 0.5f; 
+    public float speed = 10000f; 
 
     private Rigidbody2D rb;
 
@@ -14,28 +14,14 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0f;
-        
     }
 
     private void Update()
     {
-        range = range + Time.deltaTime;
-
-        if (range > 0.1f)
-        {
-            Event e = Event.current;
-            range = 0.0f;
-        }
-
-
         Vector2 movement = Input.mousePosition;
-        movement.x = Input.mousePosition.x - Screen.width / 2;
-        movement.y = Input.mousePosition.y - Screen.height / 2;
+        movement.x = (Input.mousePosition.x - Screen.width / 2) / Screen.width;
+        movement.y = (Input.mousePosition.y - Screen.height / 2) / Screen.height;
         rb.velocity = movement * speed * Time.deltaTime;
-        Debug.Log(rb.velocity);
-        Debug.Log(movement);
-        Debug.Log(speed);
-        Debug.Log(Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
