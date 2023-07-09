2d4a1e68f511a5d960c50ba36fe9d2b0e00d28c9
Gary Tang <46045360+Lelloq@users.noreply.github.com>
2023-07-09 16:21:14 +0100

    collection event works, need to add hotbar functionality

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public static event EventHandler OnPlayerDeath;
+    public static event EventHandler<OnPlayerCollectArgs> OnPlayerCollect;
+
+    public class OnPlayerCollectArgs : EventArgs
+    {
+        public Items ItemType { get; set; }
+    }
+
+    public enum Items 
+    {
+        Dynamite,
+        Topaz,
+        Diamond
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -11,6 +24,14 @@ public class Character : MonoBehaviour
         {
             // Player collided with spike
             OnPlayerDeath?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 14) 
+        {
+            OnPlayerCollect?.Invoke(this, new OnPlayerCollectArgs() {ItemType = Enum.Parse<Items>(collision.tag)});
         }
     }
 }
